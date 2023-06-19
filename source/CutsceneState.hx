f64fd51a4b852fad145154793141ff5a9d32bc5e
Dxgamer7405 <95672691+Dxgamer7405@users.noreply.github.com>
2023-06-19 19:40:53 -0400

    Gjvvjjvjv

--- source/CutsceneState.hx
+++ source/CutsceneState.hx
@@ -24,7 +24,6 @@ class CutsceneState extends MusicBeatState
 		FlxG.sound.music.stop();
 		
 		var video:FlxVideo = new FlxVideo();
-		video.play(video);
 		video.onEndReached.add(function()
   {
     video.dispose();
