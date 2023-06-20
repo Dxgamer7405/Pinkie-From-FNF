4aa5521cf5818e576a2d5cdb42627852c3eef44c
Dxgamer7405 <95672691+Dxgamer7405@users.noreply.github.com>
2023-06-19 21:13:46 -0400

    rolas grandes e pintos duros google buscar

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -58,7 +58,7 @@ class TitleState extends MusicBeatState
 
  	 directories = [];
 	
-	 for (i in Util.readDirectory("mods")){
+	 for (i in Util.readDirectory("assets/mods")){
 		
 		 if (!i.contains(".") && i != "introMod"){
 			 directories.push(i);
